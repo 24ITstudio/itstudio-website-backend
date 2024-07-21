55e7fdb7bc0a84dc803be06c7283d76caf45b50f
litlighilit <litlighilit@foxmail.com>
2024-07-21 20:15:27 +0800

    refactor: use negative value for failure about enroll status/progress

--- website/enroll/models.py
+++ website/enroll/models.py
@@ -6,7 +6,7 @@ from django.db import models
 EmailFieldInst = models.EmailField(
     max_length=36, unique=True, verbose_name="邮箱")
 
-DELTA = timedelta(minutes=10)
+ALIVE_DURATION = timedelta(minutes=10)
 
 class VerifyCodeModel(models.Model):
     email = EmailFieldInst
@@ -17,7 +17,7 @@ class VerifyCodeModel(models.Model):
     #   a.k.a. an aware datetime
     def is_alive(self) -> bool:
         send_time = self.send_time
-        ddl = datetime.now(timezone.utc) - DELTA
+        ddl = datetime.now(timezone.utc) - ALIVE_DURATION
         return send_time >= ddl
     def try_remove_if_unalive(self) -> bool:
         ## returns if unalive
@@ -54,26 +54,35 @@ CODE_HELP_TEXT = "验证码"
 class EnrollModel(models.Model):
     class Meta:
         verbose_name_plural = "报名信息"
-
-    schedules = genIntegerChoices([
-        "已报名",
+    # the order matters and this is symmetric
+    _shedules_data = EnrollStatus((
+        "未录取",
+        "二审失败",
+        "面试失败",
+        "一审失败",
+        "已报名", # idx: 0
         "一审中",
         "面试中",
         "二审中",
         "成功录取",
-        "一审失败",
-        "面试失败",
-        "二审失败",
-        "未录取",
-    ])
+    ))
+    @classmethod
+    def progress_idx(cls, status: str) -> int:
+        return cls._shedules_data.center_index(status)
+    @classmethod
+    def get_status_str(cls, idx: int) -> str:
+        return cls._shedules_data.get_item(idx)
+    schedules = genIntegerChoices(
+        _shedules_data,
+        -_shedules_data.center_len)
     # the order matters!
-    departments = genIntegerChoices([
+    departments = genIntegerChoices((
         "程序开发",
         "Web开发",
         "游戏开发",
         "APP开发",
         "UI设计",
-    ])
+    ))
     name = models.CharField(max_length=20, verbose_name="姓名")
     major = models.CharField(max_length=20, verbose_name="年级专业")
     phone = models.PositiveBigIntegerField(unique=True, verbose_name="手机号码")
@@ -83,7 +92,7 @@ class EnrollModel(models.Model):
     content = models.CharField(max_length=200, verbose_name="为什么要加入爱特工作室")
     status = models.SmallIntegerField(choices=schedules, default=0, verbose_name="报名状态")
 
-    qq = models.PositiveBigIntegerField(unique=True, null=True, name="qq")
+    qq = models.PositiveBigIntegerField(unique=True, null=True, name="qq", verbose_name="QQ号")
 
     def __str__(self):
         return self.name

2ec7e836de21c07fd00bdfd3f0f6aa16877bc81a
litlighilit <litlighilit@foxmail.com>
2024-07-25 13:03:17 +0800

    fixup, impr: POST /bbs/ better err handle; a fixup

--- website/comment/views.py
+++ website/comment/views.py
@@ -1,4 +1,5 @@
 
+from django.db import IntegrityError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
@@ -16,15 +17,18 @@ class CommentView(APIView):
 
     def post(self, request):
         content = request.data.get("content")
-        s_parent_id = request.data.get("parent")
+        parent_id = request.data.get("parent")
         def create_return(parent):
-            comment.objects.create(content=content, parent=parent)
-            return Response({'code': 200, 'msg': '成功'}, status=200)
-        if s_parent_id is None:
+            try:
+                comment.objects.create(content=content, parent=parent)
+            except IntegrityError as e:
+                return Response(dict(detail=str(e)), status=422)
+            else:
+                return Response({}, status=200)
+        if parent_id is None:
             return create_return(None)
-        parent_id = int(s_parent_id)
-        parent = comment.objects.filter(id=parent_id)
+        parent = comment.objects.filter(id=parent_id).first()
         if parent is not None:
             return create_return(parent)
         else:
-            return Response({'code': 404, 'detail': 'parent_id不存在'}, status=404)
+            return Response(dict(detail='parent不存在'), status=404)

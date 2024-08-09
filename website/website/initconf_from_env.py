4899f925098e830b14e73d8d8a50daa2d88b8a29
litlighilit <litlighilit@foxmail.com>
2024-08-09 22:30:03 +0800

    feat: log when init from env
    inner fix(unused): init_by <- chk_init_by

--- website/website/initconf_from_env.py
+++ website/website/initconf_from_env.py
@@ -3,19 +3,28 @@
 from os import getenv, pathsep
 from email.utils import parseaddr
 
-def init_by(settings_, key):
+_LOG = True  # default value
+
+def log_init_if(name, val, env_name=None, log=_LOG):
+    if log:
+        if env_name is None:
+            env_name = name
+        print(f"[settings] init {name}={val!r} from env {env_name} \t({__name__})")
+
+def init_by(settings_, key, log=_LOG) -> bool:
     "do nothing if env `key` is not set."
     val = getenv(key)
     if val is not None:
+        log_init_if(key, val, log=log)
         settings_[key] = val
-
-
-def chk_init_by(settings_, key):
-    "Raises OSError if env `key` is not set."
-    val = getenv(key)
-    if val is not None:
-        settings_[key] = val
+        return True
     else:
+        return False
+
+
+def chk_init_by(settings_, key, log=_LOG):
+    "Raises OSError if env `key` is not set."
+    if not init_by(settings_, key, log=log):
         raise OSError("the envvar '"+key+"' is not set, cannot send email")
 
 def parse_bool(s):
@@ -47,20 +56,20 @@ def parse_bool_like(val) -> bool:
         if c == '0': return False
         err()
 
-
-def init_list_env(setting_, name: str, env_name=None, mapper=None) -> bool:
+def init_list_env(setting_, name: str, env_name=None, mapper=None, log=_LOG) -> bool:
     if env_name is None: env_name = name
     val = getenv(env_name)
     if val is None: return False
     ls = val.split(pathsep)
     if mapper is not None:
         ls = list(map(mapper, ls))
+    log_init_if(name, ls, env_name=env_name, log=log)
     setting_[name] = ls
     return True
 
 DEBUG_ENV = "WEBSITE_DEBUG"
 ADMINS_ENV = "WEBSITE_ADMINS"
-def init_debug(settings_):
+def init_debug(settings_, log=_LOG):
     """get env WEBSITE_DEBUG.
     if true, then get ALLOWED_HOSTS, use os.pathsep split it.
     
@@ -71,16 +80,17 @@ def init_debug(settings_):
         return
     debug = parse_bool_like(val)
     settings_["DEBUG"] = debug
+    log_init_if("DEBUG", debug, env_name=DEBUG_ENV, log=log)
     if debug:
         return
     hosts_env = 'ALLOWED_HOSTS'
-    if not init_list_env(settings_, "ALLOWED_HOSTS"):
+    if not init_list_env(settings_, "ALLOWED_HOSTS", log=log):
         raise OSError("if not DEBUG, you must set "+hosts_env)
     
     def parse_admin(s: str):
         return parseaddr(s)
     init_list_env(settings_, "ADMINS", env_name=ADMINS_ENV,
-                  mapper=parse_admin)
+                  mapper=parse_admin, log=_LOG)
 
 
 chk_init_envs = [
@@ -92,7 +102,7 @@ init_envs = [
 ]
 
 
-def init(settings):
-    init_debug(settings)
-    for k in chk_init_envs: chk_init_by(settings, k)
-    for k in init_envs: chk_init_by(settings, k)
+def init(settings, log=_LOG):
+    init_debug(settings, log=log)
+    for k in chk_init_envs: chk_init_by(settings, k, log=log)
+    for k in init_envs: init_by(settings, k, log=log)

32924d617144c375e4e25d6bb7c15748bfe110f8
cqx <1943432515@qq.com>
2024-08-17 14:30:52 +0800

    上传

--- website/comment/models.py
+++ website/comment/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-
 class comment(models.Model):
-    content = models.TextField(verbose_name='评论内容')
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, verbose_name='父评论id')
-    datetime = models.DateTimeField(auto_now_add=True, verbose_name='评论时间')
-    qq = models.IntegerField(verbose_name='QQ号')
-    email = models.CharField(max_length=20, verbose_name='邮箱')
+    content = models.TextField(verbose_name='评论内容', default="null")
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, verbose_name='父评论id', default="null")
+    datetime = models.DateTimeField(auto_now_add=True, verbose_name='评论时间', default="null")
+    qq = models.IntegerField(verbose_name='QQ号', default="null")
+    email = models.CharField(max_length=20, verbose_name='邮箱', default="null")

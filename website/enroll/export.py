f61b2b7f6c000b42368decd0a17ac2a7c3fcfe10
litlighilit <litlighilit@foxmail.com>
2024-09-04 22:24:26 +0800

    feat(enroll): export csv's department field now is str over int

--- website/enroll/export.py
+++ website/enroll/export.py
@@ -4,14 +4,19 @@ from typing import Callable
 from django.http import HttpResponse
 from django.db.models import Model
 from django.contrib.admin import ModelAdmin
+from . import models
+from . import serializers
 
 def export_csv(self: ModelAdmin, request, queryset) -> HttpResponse:
+    Ser = serializers.EnrollSerializer
 
     meta = self.model._meta
     field_names = list(field.name for field in meta.fields)
     try: field_names.remove("id")
     except ValueError: pass
-
+    getfield = lambda obj, field: getattr(obj, field)
+    if type(queryset.first()) is models.EnrollModel:
+        getfield = lambda obj, field: Ser(obj).data.get(field, getattr(obj, field))
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
     writer = csv.writer(response)
@@ -19,7 +24,7 @@ def export_csv(self: ModelAdmin, request, queryset) -> HttpResponse:
     writer.writerow(field_names)
     for obj in queryset:
         writer.writerow(
-            getattr(obj, field) for field in field_names
+            getfield(obj, field) for field in field_names
         )
 
     return response

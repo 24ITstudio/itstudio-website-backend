96d421ed971176add193866ab4fa90e72fe05ad6
litlighilit <litlighilit@foxmail.com>
2024-09-10 15:54:21 +0800

    feat(admin): support staff see own department enrollee

--- website/enroll/admin.py
+++ website/enroll/admin.py
@@ -8,6 +8,19 @@ class VerifyCodeAdmin(admin.ModelAdmin):
     # send_time is marked as `auto_now=True`, so not editable
     #list_editable = "send_time",
 
+
+# NOTE: the order must match that of .EnrollModel.departments
+username2department = (
+    'py',
+    'web',
+    'game',
+    'app',
+    'ui',
+    'ios'
+)
+def uname2departmentIdx(name):
+    return username2department.index(name)
+
 @admin.register(models.EnrollModel)
 class EnrollAdmin(admin.ModelAdmin):
     list_display = "name", "email", "qq", "department", "status", "comment"
@@ -20,6 +33,17 @@ class EnrollAdmin(admin.ModelAdmin):
         for i in 'qq', 'content':
             form.base_fields[i].required = False
         return form
+
+    def get_queryset(self, request):
+        # 获取当前登录的用户
+        user = request.user
+        # 如果用户是超级用户，则返回所有记录
+        if user.is_superuser:
+            return super().get_queryset(request)
+        # 否则，只返回该用户名等于用户名称的记录
+        return super().get_queryset(request).filter(department=
+                                                    uname2departmentIdx(user.username)
+        )
 
 
 admin.site.add_action(export.export_csv)
